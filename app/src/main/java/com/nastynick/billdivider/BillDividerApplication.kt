60d6d77cf3ef63d46fbde378af4503a0a90f10f8
Nastacia <>
2017-11-28 00:14:39 +0400

    Added social login through Firebase

---
+++ app/src/main/java/com/nastynick/billdivider/BillDividerApplication.kt
@@ -0,0 +1,10 @@
+package com.nastynick.billdivider
+
+import android.app.Application
+
+class BillDividerApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/nastynick/billdivider/SplashActivity.kt
+++ app/src/main/java/com/nastynick/billdivider/SplashActivity.kt
@@ -2,11 +2,51 @@ package com.nastynick.billdivider
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import java.util.Arrays.asList
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.ResultCodes
+import com.firebase.ui.auth.IdpResponse
+import android.content.Intent
 
 class SplashActivity : AppCompatActivity() {
+   private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val providers = mutableListOf(
+                AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build())
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == ResultCodes.OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                // ...
+            }
+        }
+    }
+
 }
