5c83560fd65dd06c0bc56b0ce70dbdd5a1ad763d
Nastacia <>
2017-11-29 11:19:17 +0400

    Added bills view template

---
+++ app/src/main/java/com/nastynick/billdivider/bills/view/BillsAdapter.kt
@@ -0,0 +1,24 @@
+package com.nastynick.billdivider.bills.view
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class BillsAdapter : RecyclerView.Adapter<BillsAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/nastynick/billdivider/bills/view/BillsFragment.kt
@@ -0,0 +1,36 @@
+package com.nastynick.billdivider.bills.view
+
+import android.app.Fragment
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nastynick.billdivider.R
+import kotlinx.android.synthetic.main.bills_fragment.*
+
+class BillsFragment : Fragment() {
+
+    companion object {
+        fun getInstance(): BillsFragment {
+            return BillsFragment()
+        }
+    }
+
+    private val adapter = BillsAdapter()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.bills_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initBillsList()
+    }
+
+    private fun initBillsList() {
+        bills_list.layoutManager = LinearLayoutManager(activity)
+        bills_list.adapter = adapter
+    }
+}
